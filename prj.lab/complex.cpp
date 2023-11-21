9a6295b413eab744f167be6f96b9e190ed100822
Lol <denis.kalinkin.05@gmail.com>
2023-11-21 16:57:06 +0300

    Update complex.cpp

--- prj.lab/complex.cpp
+++ prj.lab/complex.cpp
@@ -1,118 +1,28 @@
-#include <iostream>
-#include <sstream>
+#include "complex.h"
 
-struct Complex {
-    Complex() {}
-    explicit Complex(const double real);
-    Complex(const double real, const double imaginary);
-    bool operator==(const Complex& rhs) const { return (re == rhs.re) && (im == rhs.im); }
-    bool operator!=(const Complex& rhs) const { return !operator==(rhs); }
-    Complex& operator+=(const Complex& rhs);
-    Complex& operator+=(const double rhs) { return operator+=(Complex(rhs)); }
-    Complex& operator-=(const Complex& rhs);//                                           <------------
-    Complex& operator-=(const double rhs) { return operator-=(Complex(rhs)); }//<------------
-    //Complex& operator*=(const Complex& rhs);//                                         <------------
-    Complex& operator*=(const double rhs);
-    std::ostream& writeTo(std::ostream& ostrm) const;
-    std::istream& readFrom(std::istream& istrm);
+Complex operator+(const Complex& lhs, const Complex& rhs) {Complex new_obj; new_obj += lhs; new_obj += rhs; return new_obj;}
+Complex operator+(const Complex& lhs, const double& rhs) {Complex new_obj; new_obj += lhs; new_obj += rhs; return new_obj;}
+Complex operator+(const double& lhs, const Complex& rhs) {Complex new_obj; new_obj += lhs; new_obj += rhs; return new_obj;}
 
-    double re{ 0.0 };
-    double im{ 0.0 };
+Complex operator-(const Complex& lhs, const Complex& rhs) {Complex new_obj = lhs; new_obj -= rhs; return new_obj;}
+Complex operator-(const Complex& lhs, const double& rhs) {Complex new_obj = lhs; new_obj -= rhs; return new_obj;}
+Complex operator-(const double& lhs, const Complex& rhs) {Complex new_obj; new_obj += lhs; new_obj -= rhs; return new_obj;}
 
-    static const char leftBrace{ '{' };
-    static const char separator{ ',' };
-    static const char rightBrace{ '}' };
-};
+Complex operator*(const Complex& lhs, const Complex& rhs) {Complex new_obj = lhs; new_obj *= rhs; return new_obj;}
+Complex operator*(const Complex& lhs, const double& rhs) {Complex new_obj = lhs; new_obj *= rhs; return new_obj;}
+Complex operator*(const double& lhs, const Complex& rhs) {Complex new_obj = rhs; new_obj *= lhs; return new_obj;}
 
-Complex operator+(const Complex& lhs, const Complex& rhs);
-Complex operator-(const Complex& lhs, const Complex& rhs);
 
-inline std::ostream& operator<<(std::ostream& ostrm, const Complex& rhs)
-{
-    return rhs.writeTo(ostrm);
-}
+Complex operator/(const Complex& lhs, const Complex& rhs) {Complex new_obj = lhs; new_obj /= rhs; return new_obj;}
+Complex operator/(const Complex& lhs, const double& rhs) {Complex new_obj = lhs; new_obj /= rhs; return new_obj;}
+Complex operator/(const double& lhs, const Complex& rhs) {Complex new_obj(lhs); new_obj /= rhs; return new_obj;}
 
-inline std::istream& operator>>(std::istream& istrm, Complex& rhs)
-{
-    return rhs.readFrom(istrm);
-}
-
-bool testParse(const std::string& str)
-{
-    using namespace std;
-    istringstream istrm(str);
-    Complex z;
-    istrm >> z;
-    if (istrm.good()) {
-        cout << "Read success: " << str << " -> " << z << endl;
-    } else {
-        cout << "Read error : " << str << " -> " << z << endl;
-    }
-    return istrm.good();
-}
-
-int main()
-{
-    using namespace std;
-
-    Complex z;
-    z += Complex(8.0);
-    testParse("{8.9, 9}");
-    testParse("{8.9, 9}");
-    testParse("{8.9, 9}");
-    return 0;
-}
-Complex::Complex(const double real)
-        : Complex(real, 0.0)
-{
-}
-
-Complex::Complex(const double real, const double imaginary)
-        : re(real)
-        , im(imaginary)
-{
-}
-
-Complex& Complex::operator+=(const Complex& rhs)
-{
-    re += rhs.re;
-    im += rhs.im;
-    return *this;
-}
-
-Complex& Complex::operator-=(const Complex& rhs) {
-    re -= rhs.re;
-    im -= rhs.im;
-    return *this;
-}
-
-Complex operator+(const Complex& lhs, const Complex& rhs)
-{
-    Complex sum(lhs);
-    sum += rhs;
-    return sum;
-}
-
-Complex operator-(const Complex& lhs, const Complex& rhs)
-{
-    return Complex(lhs.re - rhs.re, lhs.im - rhs.im);
-}
-
-Complex& Complex::operator*=(const double rhs)
-{
-    re *= rhs;
-    im *= rhs;
-    return *this;
-}
-
-std::ostream& Complex::writeTo(std::ostream& ostrm) const
-{
+std::ostream& Complex::writeTo(std::ostream& ostrm) const{
     ostrm << leftBrace << re << separator << im << rightBrace;
     return ostrm;
 }
 
-std::istream& Complex::readFrom(std::istream& istrm)
-{
+std::istream& Complex::readFrom(std::istream& istrm){
     char leftBrace(0);
     double real(0.0);
     char comma(0);
@@ -129,3 +39,45 @@ std::istream& Complex::readFrom(std::istream& istrm)
     }
     return istrm;
 }
+
+//inline std::ostream& operator<<(std::ostream& ostrm, const Complex& rhs){
+//    return rhs.writeTo(ostrm);
+//}
+//
+//inline std::istream& operator>>(std::istream& istrm, Complex& rhs){
+//    return rhs.readFrom(istrm);
+//}
+
+Complex& Complex::operator+=(const Complex& rhs){
+
+    re += rhs.re;
+    im += rhs.im;
+    return *this;
+}
+
+Complex& Complex::operator-=(const Complex& rhs){
+    re -= rhs.re;
+    im -= rhs.im;
+    return *this;
+}
+
+Complex& Complex::operator*=(const Complex& rhs){
+
+    double a = re, b = im, c = rhs.re, d = rhs.im;
+    re = a * c - b * d;
+    im = a * d + b * c;
+    return *this;
+}
+
+Complex& Complex::operator/=(const Complex& rhs){
+
+    double a = re, b = im, c = rhs.re, d = rhs.im;
+    if (c == 0 && d==0){
+        throw std::runtime_error("Division by zero");
+    }
+    re = (a * c + b * d) / (c * c + d * d);
+    im = (b * c - a * d) / (c * c + d * d);
+    return *this;
+}
+
+
---
+++ prj.lab/test_complex.cpp
@@ -0,0 +1,35 @@
+#include "complex.h"
+
+bool testParse(const std::string& str){
+    std::istringstream istrm(str);
+    Complex z;
+    istrm >> z;
+    if (istrm.good()){
+        std::cout << "Read succes: " << str << "->" << z << std::endl;
+    }else{
+        std::cout << "Read error: " << str << "->" << z << std::endl;
+    }
+    return istrm.good();
+}
+
+//bool testSum()
+bool testSum(double a, Complex b, Complex c){
+    Complex testans1 = b + c;
+    Complex testans2 = b + a;
+    Complex testans3 = a + c;
+    std::cout << testans1 << std::endl;
+    std::cout << testans2 << std::endl;
+    std::cout << testans3 << std::endl;
+    return true; //пока что заглушка
+}
+//дописать тесты
+
+int main(){
+    Complex z(8.0);
+    Complex x({4.0, 5});
+    double y = 9.0;
+    testSum(y, z, x);
+    testParse("{1.4, 9}");
+    testParse("{8.9, 9}");
+    return 0;
+}
